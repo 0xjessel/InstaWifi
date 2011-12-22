2207532ac2be92fcec9a4f2ad9ea6ee243352aa8
Jesse Chen <github@jessechen.net>
2011-12-21 19:29:07 -0800

    connect to wifi from NFC intent method

--- src/net/jessechen/instawifi/InstaWifiHandler.java
+++ src/net/jessechen/instawifi/InstaWifiHandler.java
@@ -48,23 +48,27 @@ public class InstaWifiHandler extends Activity {
 		WifiConfiguration mWc = new WifiConfiguration();
 		mWc.SSID = ssid;
 		mWc.status = WifiConfiguration.Status.DISABLED;
-		
-		if (protocol.equals("")) { 
+
+		// thanks to:
+		// http://kmansoft.com/2010/04/08/adding-wifi-networks-to-known-list/
+		if (protocol.equals("")) { // TODO: something like this, need to check
 			// open network configs
-			
+
 			mWc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
 			mWc.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
 			mWc.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
 			mWc.allowedAuthAlgorithms.clear();
-			mWc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
-			mWc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+			mWc.allowedPairwiseCiphers
+					.set(WifiConfiguration.PairwiseCipher.CCMP);
+			mWc.allowedPairwiseCiphers
+					.set(WifiConfiguration.PairwiseCipher.TKIP);
 			mWc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
 			mWc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
 			mWc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
 			mWc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
 		} else if (protocol.equals("WEP")) {
 			// WEP network configs
-			
+
 			mWc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
 			mWc.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
 			mWc.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
@@ -86,19 +90,20 @@ public class InstaWifiHandler extends Activity {
 			mWc.wepTxKeyIndex = 0;
 		} else if (protocol.equals("WPA")) {
 			// WPA network configs
-			
+
 			mWc.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
 			mWc.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
 			mWc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
-			mWc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
-			mWc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+			mWc.allowedPairwiseCiphers
+					.set(WifiConfiguration.PairwiseCipher.CCMP);
+			mWc.allowedPairwiseCiphers
+					.set(WifiConfiguration.PairwiseCipher.TKIP);
 			mWc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
 			mWc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
 			mWc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
 			mWc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
 
 			mWc.preSharedKey = "\"".concat(pw).concat("\"");
-
 		}
 
 		// add network to known list
@@ -106,6 +111,8 @@ public class InstaWifiHandler extends Activity {
 		if (netId != -1) {
 			mWm.enableNetwork(netId, true);
 			Log.i(Util.TAG, "attemping to connect to new network");
+			// TODO: needs a broadcast receiver on
+			// WifiManager.SUPPLICANT_STATE_CHANGED_ACTION
 		} else {
 			Log.i(Util.TAG,
 					"netId == -1, failed to add network to known networks");
