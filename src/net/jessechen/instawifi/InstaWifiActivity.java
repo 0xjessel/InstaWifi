fab0603ea0087110234f48a996cc82d59a0ae56d
Jesse Chen <github@jessechen.net>
2011-12-22 09:14:08 -0800

    took out nfc p2p code

--- src/net/jessechen/instawifi/InstaWifiActivity.java
+++ src/net/jessechen/instawifi/InstaWifiActivity.java
@@ -7,19 +7,17 @@ import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.IntentFilter.MalformedMimeTypeException;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.view.View;
 
 public class InstaWifiActivity extends Activity {
-	private boolean mResumed = false;
 	private boolean mWriteMode = false;
 	NfcAdapter mNfcAdapter;
 
 	PendingIntent mNfcPendingIntent;
 	IntentFilter[] mWriteTagFilters;
-	IntentFilter[] mNdefExchangeFilters;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -34,26 +32,26 @@ public class InstaWifiActivity extends Activity {
 		mNfcPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
 				getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
 
-		// Intent filters for reading a note from a tag or exchanging over p2p.
-		IntentFilter ndefDetected = new IntentFilter(
-				NfcAdapter.ACTION_NDEF_DISCOVERED);
-		try {
-			ndefDetected.addDataType("text/plain");
-		} catch (MalformedMimeTypeException e) {
-		}
-		mNdefExchangeFilters = new IntentFilter[] { ndefDetected };
-
 		// Intent filters for writing to a tag
 		IntentFilter tagDetected = new IntentFilter(
 				NfcAdapter.ACTION_TAG_DISCOVERED);
 		mWriteTagFilters = new IntentFilter[] { tagDetected };
 	}
 
+	@Override
+	protected void onNewIntent(Intent intent) {
+		if (mWriteMode
+				&& NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+			Tag detectedTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+			NfcUtil.writeTag(NfcUtil.getWifiAsNdef(null, null, null),
+					detectedTag, this);
+		}
+	}
+
 	private View.OnClickListener mTagWriter = new View.OnClickListener() {
 		@Override
 		public void onClick(View arg0) {
 			// Write to a tag for as long as the dialog is shown.
-			disableNdefExchangeMode();
 			enableTagWriteMode();
 
 			new AlertDialog.Builder(InstaWifiActivity.this)
@@ -63,23 +61,10 @@ public class InstaWifiActivity extends Activity {
 								@Override
 								public void onCancel(DialogInterface dialog) {
 									disableTagWriteMode();
-									enableNdefExchangeMode();
 								}
 							}).create().show();
 		}
 	};
-
-	private void enableNdefExchangeMode() {
-		mNfcAdapter.enableForegroundNdefPush(InstaWifiActivity.this,
-				NfcUtil.getWifiAsNdef(null, null, null));
-		mNfcAdapter.enableForegroundDispatch(this, mNfcPendingIntent,
-				mNdefExchangeFilters, null);
-	}
-
-	private void disableNdefExchangeMode() {
-		mNfcAdapter.disableForegroundNdefPush(this);
-		mNfcAdapter.disableForegroundDispatch(this);
-	}
 
 	private void enableTagWriteMode() {
 		mWriteMode = true;
