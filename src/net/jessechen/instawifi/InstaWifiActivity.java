411efce3dc247cf46b5552b441fdc2895040e876
Jesse Chen <github@jessechen.net>
2012-01-05 15:13:09 -0800

    remove test code

--- src/net/jessechen/instawifi/InstaWifiActivity.java
+++ src/net/jessechen/instawifi/InstaWifiActivity.java
@@ -53,7 +53,7 @@ public class InstaWifiActivity extends Activity {
 			WifiModel currentWifi = WifiUtil.getCurrentWifiModel(this);
 			if (WifiUtil.isValidWifiModel(currentWifi)) {
 				NdefMessage wifiNdefMessage = NfcUtil.getWifiAsNdef(
-						currentWifi.getSSID(), "123123123",
+						currentWifi.getSSID(), currentWifi.getPassword(),
 						currentWifi.getProtocol());
 				if (NfcUtil.writeTag(wifiNdefMessage, detectedTag, this)) {
 					Log.i(Util.TAG, String.format(
