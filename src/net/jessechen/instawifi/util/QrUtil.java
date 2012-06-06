4b755270ab2903ef3ee2fec95fb561211d740987
Jesse Chen <github@jessechen.net>
2012-06-06 12:44:59 -0700

    add promotional/screenshots pictures and modify filename of qr code sharing and gmail text

--- src/net/jessechen/instawifi/util/QrUtil.java
+++ src/net/jessechen/instawifi/util/QrUtil.java
@@ -66,7 +66,7 @@ public class QrUtil {
 	
 	public static String getQrFilename(String ssid) {
 		if (ssid != null) {
-			return "InstaWifi" + ssid + " QR" + ".jpg";
+			return "InstaWifi " + ssid + " QR" + ".jpg";
 		} else {
 			throw new NullPointerException("null ssid");
 		}
