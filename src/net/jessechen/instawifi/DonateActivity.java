8c5f07676dc65c6978fa757ca8f472aa2f499e39
Jesse Chen <github@jessechen.net>
2012-07-18 00:48:07 -0700

    touching up DonateActivity, just need to wire up the components
    #1 WIP

--- src/net/jessechen/instawifi/DonateActivity.java
+++ src/net/jessechen/instawifi/DonateActivity.java
@@ -1,14 +1,43 @@
 package net.jessechen.instawifi;
 
-import net.jessechen.instawifi.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class DonateActivity extends Activity {
+public class DonateActivity extends Activity implements OnClickListener,
+		RadioGroup.OnCheckedChangeListener {
+	private RadioGroup donateOptions;
+	private Button donateButton;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		donateButton = (Button) findViewById(R.id.donate_button);
+		donateOptions = (RadioGroup) findViewById(R.id.donate_options);
+
+
 		setContentView(R.layout.donate_activity);
 	}
+
+	@Override
+	public void onClick(View v) {
+		if (v == donateButton) {
+			// launch android billing service here
+		}
+	}
+
+	@Override
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		RadioButton checkedRadioButton = (RadioButton) donateOptions.findViewById(checkedId);
+		if (checkedRadioButton.isChecked()) {
+			donateButton.setText(String.format(getString(R.string.donate_button),
+					checkedRadioButton.toString()));
+		}
+	}
+
 }
